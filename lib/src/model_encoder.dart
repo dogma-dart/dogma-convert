c53a47ba78c05107934f1eca0fcb7c066e0bf3ca
Don <don.j.olmstead@gmail.com>
2015-09-01 23:54:46 -0700

    Moving to Drone 0.4

---
+++ lib/src/model_encoder.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2015, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+/// Contains the [ModelEncoder] interface.
+library dogma_data.src.model_encoder;
+
+//---------------------------------------------------------------------
+// Standard libraries
+//---------------------------------------------------------------------
+
+import 'dart:convert';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Interface for encoding a [Model] from a [Map].
+///
+/// The [ModelEncoder] takes a [Model] as input and converts it from a Plain
+/// Old Dart Object (PODO) to a [Map]. Its behavior is defined by an explicit
+/// or implicit serialization on the [Model].
+abstract class ModelEncoder<Model> implements Converter<Model, Map> {}
