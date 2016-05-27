5122555b635036ca240c6a3475d86d9a7ee634f3
Don <don.j.olmstead@gmail.com>
2016-05-26 22:36:11 -0700

    Clone coverage locally for now

--- build.sh
+++ build.sh
@@ -10,8 +10,11 @@ pub install
 # Verify that the libraries are error and warning-free.
 dartanalyzer ${DARTANALYZER_FLAGS} $(ls -rt lib/*.dart)
 
+# Clone coverage till repo is fixed
+git clone -b migration https://github.com/dart-lang/coverage.git ../coverage
+
 # Run the tests
-pub global activate coverage
+pub global activate --source path ../coverage
 OBSERVATORY_PORT=8000
 COVERAGE_OUTPUT=coverage.json
 
